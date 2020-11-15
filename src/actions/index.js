0f124bdc044e4bfe5f3f24e5d92842da84435546
moksh <moksh@source.one>
2020-11-15 13:40:24 +0530

    wired up the redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => {
+  return async function (dispatch, getState) {
+    const res = await jsonPlaceholder.get("/posts");
+    dispatch({
+      type: "FETCH_POSTS",
+      payload: res,
+    });
+  };
+};
