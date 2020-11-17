0ed3ca784b0204ddddfec22af2b85fb3d4e98544
moksh <moksh@source.one>
2020-11-17 19:36:10 +0530

    memoized using lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => {
@@ -11,9 +12,13 @@ export const fetchPosts = () => {
 };
 
 export const fetchUser = (id) => async (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder(`/users/${id}`);
   dispatch({
     type: "FETCH_USER",
     payload: response.data,
   });
-};
+});
